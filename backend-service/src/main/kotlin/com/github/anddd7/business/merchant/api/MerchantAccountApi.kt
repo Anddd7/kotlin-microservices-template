8b1d6fcb9a46211190b4af04c73d19a46c1e818b
Andong Liao <adliao@thoughtworks.com>
2021-11-06 14:07:16 +0800

    merchant onboarding: get proposal (minor change

--- backend-service/src/main/kotlin/com/github/anddd7/business/merchant/api/MerchantAccountApi.kt
+++ backend-service/src/main/kotlin/com/github/anddd7/business/merchant/api/MerchantAccountApi.kt
@@ -2,7 +2,6 @@ package com.github.anddd7.business.merchant.api
 
 import com.github.anddd7.business.merchant.service.ProposalService
 import com.github.anddd7.business.merchant.service.request.ProposalRequest
-import com.github.anddd7.business.merchant.service.request.ProposalRequestResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,10 +9,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/merchant-account")
 class MerchantAccountApi(
-    private val proposalEventHandler: ProposalService
+    private val proposalService: ProposalService
 ) {
-    @GetMapping("proposal")
-    fun proposal(): ProposalRequestResponse = proposalEventHandler.request(ProposalRequest())
+    @GetMapping("/proposal")
+    fun proposal() = proposalService.request(ProposalRequest())
 }
 
 
