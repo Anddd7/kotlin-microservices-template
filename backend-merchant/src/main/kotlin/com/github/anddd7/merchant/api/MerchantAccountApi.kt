5af3441458f5d97f54b371ae70bed2cf56a0ecba
Andong Liao <adliao@thoughtworks.com>
2021-11-24 18:17:40 +0800

    new implementation of the api

--- backend-merchant/src/main/kotlin/com/github/anddd7/merchant/api/MerchantAccountApi.kt
+++ backend-merchant/src/main/kotlin/com/github/anddd7/merchant/api/MerchantAccountApi.kt
@@ -1,7 +1,6 @@
 package com.github.anddd7.merchant.api
 
 import com.github.anddd7.merchant.service.MerchantAccountService
-import com.github.anddd7.merchant.domain.user.RegisteredUserService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,13 +10,12 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/merchant-account")
 class MerchantAccountApi(
-    private val registeredUserService: RegisteredUserService,
     private val merchantAccountService: MerchantAccountService,
 ) {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create() {
-
-
+        merchantAccountService.create()
     }
 }
+
--- backend-merchant/src/main/kotlin/com/github/anddd7/merchant/service/MerchantAccountService.kt
+++ backend-merchant/src/main/kotlin/com/github/anddd7/merchant/service/MerchantAccountService.kt
@@ -1,8 +1,60 @@
 package com.github.anddd7.merchant.service
 
+import com.github.anddd7.merchant.domain.user.RegisteredUser
+import com.github.anddd7.merchant.domain.user.RegisteredUserService
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
+import java.time.Instant
 
 @Service
-class MerchantAccountService {
-
+class MerchantAccountService(
+    private val registeredUserService: RegisteredUserService,
+    private val handler: MerchantAccountHandler,
+) {
+    fun create() {
+        val currentUser = registeredUserService.getCurrentUser()
+        val createAt = Instant.now()
+        val command = CreateMerchantAccount(currentUser, createAt, createAt)
+        handler.create(command)
+    }
 }
+
+@Component
+class MerchantAccountHandler(
+    private val storage: MerchantAccountStorage
+) {
+    fun create(command: CreateMerchantAccount) {
+        val entity = command.run {
+            MerchantAccount(createdBy = user.name, createdAt = createAt, expiredAt = expiredAt)
+        }
+        storage.save(entity)
+    }
+}
+
+interface MerchantAccountStorage {
+    fun save(entity: MerchantAccount)
+}
+
+@Component
+class MerchantAccountRepository: MerchantAccountStorage {
+    override fun save(entity: MerchantAccount){
+        // TODO save to db or memory
+    }
+}
+
+interface Command
+interface Entity
+
+data class CreateMerchantAccount(
+    val user: RegisteredUser,
+    val createAt: Instant,
+    val expiredAt: Instant,
+) : Command
+
+data class MerchantAccount(
+    val id: Long = 0,
+    val createdBy: String,
+    val createdAt: Instant,
+    val expiredAt: Instant,
+) : Entity
